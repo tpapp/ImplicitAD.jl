ac12cd2277f69c34dbfba2f94aad6939cb95ea26
Tamás K. Papp <tkpapp@gmail.com>
2022-05-03 15:48:11 +0200

    comment about inference failure and debugging

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,10 +9,16 @@ const ϵ = eps()^0.25
         β = @inferred one_one(α)
         @test one_one_core(α, β) ≈ 0 atol = √eps()
         test_rrule(one_one, α;
+                   # when https://github.com/JuliaDiff/ChainRulesTestUtils.jl/issues/246
+                   # is fixed, remove:
                    check_inferred = false,
                    fdm = forward_fdm(5, 1),
                    atol = ϵ, rtol = ϵ)
     end
 end
 
-# @inferred rrule(ChainRulesTestUtils.TestConfig(), one_one, 1.0)
+# v, pb = @inferred rrule(ChainRulesTestUtils.TestConfig(), one_one, 1.0)
+# @code_warntype pb(1)
+# using ChainRulesCore
+# g, pbg = @inferred rrule_via_ad(ChainRulesTestUtils.TestConfig(), one_one_core, 1.0, 2.0)
+# @code_warntype pbg(1.0)
